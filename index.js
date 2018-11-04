31257859bde3656bcd81a8268744796f72acce44
sotek222 <sotek222@yahoo.com>
2018-11-04 03:38:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,11 +10,22 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  let newItem = {
+    itemName: item,
+    itemPrice: (Math.floor(Math.random() * 100)),
+  }
+  cart.push(newItem);
+  return `${newItem.itemName} has been added to your cart.`;
 }
 
 function viewCart() {
-  // write your code here
+  for(let i=0;i<cart.length;i++){
+    if(cart.length === 1) {
+      return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`
+    } else if(cart.length === 2){
+      return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}.`
+    }
+  }
 }
 
 function total() {
