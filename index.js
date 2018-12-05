0ba41e98ff5e3c9420b613908ca9c56aafbee09e
sotek222 <sotek222@yahoo.com>
2018-12-05 22:42:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
 var cart = [];
 
 function getCart() {
- return cart;
 }
 
 function setCart(c) {
-  cart = c;
-  return cart;
 }
 
 function addToCart(item) {
- // write your code here
+  let price = Math.floor(Math.random() * 100 + 1);
+  cart.push({itemName: item, itemPrice: price});
+  return `${item} has been added to your cart.`
 }
 
 function viewCart() {
-  // write your code here
 }
 
 function total() {
